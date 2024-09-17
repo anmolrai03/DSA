5bb28845ab673089528c753345240ab739b93338
Anmol Rai <purevoidexists@gmail.com>
2024-09-17 07:50:31 +0530

    Maximum consecutive Ones

---
+++ src/Arrays/MaxConsiOnes.java
@@ -0,0 +1,28 @@
+package Arrays;
+
+public class MaxConsiOnes {
+    public static void main(String[] args) {
+        int [] arr = {1,1,1, 0 , 0 , 0 , 1, 0 , 0 , 1, 1,1,1};
+        System.out.println(consecutiveOnes(arr.length, arr));
+    }
+
+    public static int consecutiveOnes(int n, int[] arr) {
+		// Write your code here.
+        int maxi = 0;
+        int i = 0;
+        int count = 0;
+        while(i<n){
+            if(arr[i] == 1){
+                count++;
+                if(count > maxi ){
+                    maxi = count;
+                }
+            } else {
+                count = 0;
+            }
+            i++;
+        }
+        
+        return maxi;
+	}
+}
