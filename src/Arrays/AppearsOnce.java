6f20aa3e7c916de3509227897e54ad8003fc19ed
Anmol Rai <purevoidexists@gmail.com>
2024-09-17 07:50:15 +0530

    Number that appears once in an array

---
+++ src/Arrays/AppearsOnce.java
@@ -0,0 +1,18 @@
+package Arrays;
+
+public class AppearsOnce {
+    public static void main(String[] args) {
+        int[] arr = {1,1, 2, 3,3, 4,4, 5, 5};
+        System.out.println(getSingleElement(arr));
+    }
+
+    public static int getSingleElement(int []arr){
+        // Write your code here.
+        //optimal 
+        int XOR = 0;
+        for(int i = 0 ; i< arr.length ; i++){
+            XOR ^= arr[i];
+        }
+        return XOR;
+    }
+}
