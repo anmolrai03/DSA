71ea0ed257fa70d6b6afa30695a50ed4926d326a
Anmol Rai <purevoidexists@gmail.com>
2024-09-24 23:12:23 +0530

    Stocks buy and sell

---
+++ src/Arrays/Medium/BuySell.java
@@ -0,0 +1,20 @@
+package Arrays.Medium;
+
+public class BuySell {
+
+    public static int maxProfit(int[] prices) {
+        int mini = prices[0];
+        int profit = 0;
+        for(int i = 1 ; i < prices.length ;i++){
+            int presProfit = prices[i] - mini;
+            profit = Math.max(profit , presProfit);
+            mini = Math.min(mini , prices[i]);
+        }
+        return profit;
+    }
+    public static void main(String[] args) {
+        int[] prices = {7,1,5,3,6,4};
+        System.out.println(maxProfit(prices));
+    }
+    
+}
